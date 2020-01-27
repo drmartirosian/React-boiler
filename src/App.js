3d4d02448efd08f78bbf0dd2e17263103acab970
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-27 09:02:06 -0600

    First

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Newcomp from './components/Newcomp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // //POSSIBLE STATES
+  // state = {
+  //   isTrue: false,
+  //   isToggled: false,
+  // }
+  // //DONE/SUB BUTTON CHANGER
+  // handleSubmit = () =>{
+  //   this.setState({
+  //     isTrue: !this.state.isTrue,
+  //   });
+  // };
+  // //TOGGLE BUTTON
+  // handleToggle = () =>{
+  //   this.setState({
+  //     isToggled: !this.state.isToggled,
+  //   });
+  // };
+
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+      {/* NEW STUFF */}
+      <Newcomp/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Newcomp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Newcomp = (props) => (
+    <div>
+        <h1>TEST COMP...</h1>
+    </div>
+  );
+
+// //BUTTON FUNCTIOM
+// function Button({ label, handleClick }){
+//     return <button onClick={handleClick} >{label}</button>
+// }
+// Button.protoTypes = {
+//     label: PropTypes.string.isRequired,
+//     handleClick: PropTypes.func.isRequired,
+// }
+
+export default Newcomp
